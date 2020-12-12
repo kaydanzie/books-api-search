05bfef2719d2eeb494483b774cae938630de60e5
Kayla Ziegler <kaydanzie@gmail.com>
2020-12-12 17:36:52 -0500

    Requires the user to pass the search param, since it's required by the external API

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,23 @@
 class BooksController < ApplicationController
   # GET /api/v1/books
   def index
-    if BookSearch.exists?(query_params: request.query_parameters)
+    if request.query_parameters['search'].blank?
+      render_missing_param("search")
+    elsif BookSearch.exists?(query_params: request.query_parameters)
       api_response = BookSearch.find_by(query_params: request.query_parameters).api_data
+      render json: api_response, status: :ok
     else
       api_response = BookService.new.works(request.query_parameters)
       # TODO: Add logic to handle errors when creating BookSearch
       BookSearch.create(query_params: request.query_parameters, api_data: api_response)
+      render json: api_response, status: :ok
     end
+  end
 
-    render json: api_response, status: :ok
+  private
+
+  def render_missing_param(required_param)
+    render json: { error: "'#{required_param}' query parameter is required" },
+           status: :bad_request
   end
 end
