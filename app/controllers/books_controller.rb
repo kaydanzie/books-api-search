cdb61c2436a2b0c6a2c5cbb5f8dad1f60f35598c
Kayla Ziegler <kaydanzie@gmail.com>
2020-12-12 17:45:30 -0500

    Moves some logic outside the index action to make it more readable

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
+  before_action :set_book_search
+
   # GET /api/v1/books
   def index
     if request.query_parameters['search'].blank?
       render_missing_param("search")
-    elsif BookSearch.exists?(query_params: request.query_parameters)
-      api_response = BookSearch.find_by(query_params: request.query_parameters).api_data
-      render json: api_response, status: :ok
+    elsif @book_search.present?
+      render json: @book_search.api_data, status: :ok
     else
       api_response = BookService.new.works(request.query_parameters)
       # TODO: Add logic to handle errors when creating BookSearch
@@ -19,5 +20,9 @@ class BooksController < ApplicationController
   def render_missing_param(required_param)
     render json: { error: "'#{required_param}' query parameter is required" },
            status: :bad_request
+  end
+
+  def set_book_search
+    @book_search = BookSearch.find_by(query_params: request.query_parameters)
   end
 end
