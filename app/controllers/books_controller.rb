a411ad438a1c5a2279cdc143972508220c87a011
Kayla Ziegler <kaydanzie@gmail.com>
2020-12-12 17:23:59 -0500

    Expands the index action to look up BookSearches by query_params and use the result if it exists, otherwise call the API and create a new BookSearch.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,14 @@
 class BooksController < ApplicationController
   # GET /api/v1/books
   def index
-    api_response = BookService.new.works(request.query_parameters)
-    render json: api_response
+    if BookSearch.exists?(query_params: request.query_parameters)
+      api_response = BookSearch.find_by(query_params: request.query_parameters).api_data
+    else
+      api_response = BookService.new.works(request.query_parameters)
+      # TODO: Add logic to handle errors when creating BookSearch
+      BookSearch.create(query_params: request.query_parameters, api_data: api_response)
+    end
+
+    render json: api_response, status: :ok
   end
 end
