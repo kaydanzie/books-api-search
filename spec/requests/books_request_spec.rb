615a07b0f9d84a0e6e1d5ca79830500ee22246de
Kayla Ziegler <kaydanzie@gmail.com>
2020-12-12 17:24:35 -0500

    Adds test that ensure BookSearches are created if they don't exist, and they replace a call to the external API.

--- spec/requests/books_request_spec.rb
+++ spec/requests/books_request_spec.rb
@@ -15,5 +15,39 @@ RSpec.describe "Books", type: :request do
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body)).to have_key("works")
     end
+
+    it 'creates a BookSearch' do
+      # Arrange
+      stub_works_api
+
+      # Act & Assert
+      expect { 
+        get books_path(params: { search: "The Great Gatsby" })
+      }.to change(BookSearch, :count).by(1)
+    end
+
+    context 'if BookSearch with query params already exists' do
+      let(:params) { { search: "The Great Gatsby" } }
+
+      it "doesn't create a new BookSearch" do
+        # Arrange
+        BookSearch.create(query_params: params)
+
+        # Act & Assert
+        expect { get books_path(params: params) }.not_to change(BookSearch, :count)
+      end
+
+      it "doesn't make a request to the external API" do
+        # Arrange
+        BookSearch.create(query_params: params)
+        api_request = stub_works_api
+
+        # Act
+        get books_path(params: params)
+
+        # Assert
+        expect(api_request).not_to have_been_requested
+      end
+    end
   end
 end
